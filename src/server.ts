e38d1380851a90d65d3a61bd1cd7bed1104e2e5e
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-05-16 22:25:40 +0600

    write an api for delete user from users table in postgresql with neon serverless cloud and also tested successfully with postman

--- src/server.ts
+++ src/server.ts
@@ -128,26 +128,54 @@ app.put("/api/users/:id", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(
       `
-      UPDATE users SET name=$1 , password=$2 , age=$3 , is_active=$4 , updated_at=NOW() WHERE id=$5 RETURNING *
+      UPDATE users SET name=COALESCE($1 , name) , password=COALESCE($2 ,password) , age=COALESCE($3 ,age) , is_active=COALESCE($4, is_active) , updated_at=NOW() WHERE id=$5 RETURNING *
       `,
       [name, password, age, is_active, id],
     );
-    if(result.rowCount === 0){
+    if (result.rowCount === 0) {
       res.status(404).json({
-        success : false ,
-        message : "User not found"
-      })
+        success: false,
+        message: "User not found",
+      });
     }
     res.status(200).json({
-      success : true,
-      message : "User updated successfully",
-      user : result.rows[0]
-    })
-  } catch (error : any) {
+      success: true,
+      message: "User updated successfully",
+      user: result.rows[0],
+    });
+  } catch (error: any) {
     res.status(500).json({
-      success : false ,
-      message : "Something went wrong"
-    })
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+});
+
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+      DELETE FROM users WHERE id=$1 
+      `,
+      [id],
+    );
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+      error,
+    });
   }
 });
 
