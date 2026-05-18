0fe8d3364a806e3f0632360892f156b24f3ed043
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-05-19 00:55:42 +0600

    implement moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,13 @@ import express, {
   type Response,
 } from "express";
 import { pool } from "./db";
+import { userRoute } from "./modules/user/user.route";
 
 const app: Application = express();
 
 app.use(express.json()); //midleware
 app.use(express.text()); //midleware
 app.use(express.urlencoded({ extended: true })); // midleware
-
 
 app.get("/", (req: Request, res: Response) => {
   // res.send("This is express servers");
@@ -19,6 +19,8 @@ app.get("/", (req: Request, res: Response) => {
     Author: "Mehraj",
   });
 });
+
+app.use("/api/users", userRoute);
 
 app.get("/api/users", async (req: Request, res: Response) => {
   try {
@@ -70,28 +72,7 @@ app.get("/api/users/:id", async (req: Request, res: Response) => {
   }
 });
 
-app.post("/api/users", async (req: Request, res: Response) => {
-  // console.log(req.body);
-  const { name, email, password, age } = req.body;
 
-  try {
-    const result = await pool.query(
-      `
-    INSERT INTO users (name , email , password , age) VALUES($1,$2,$3,$4) RETURNING *
-    `,
-      [name, email, password, age],
-    );
-    res.status(201).json({
-      message: "User created successfully",
-      data: result.rows[0],
-    });
-  } catch (error: any) {
-    res.status(500).json({
-      message: error.message,
-      error,
-    });
-  }
-});
 
 app.put("/api/users/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,21 @@
+import type { Request, Response } from "express";
+import { userService } from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userService.createUserInDB(req.body);
+    res.status(201).json({
+      message: "User created successfully",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      message: error.message,
+      error,
+    });
+  }
+};
+
+export const userController = {
+  createUser,
+};
