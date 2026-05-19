967572c6f9bc829eebcb83bba1f2f012ca1f2aed
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-05-19 16:42:04 +0600

    creating user profiles in pg with neon and tested with postman successfully

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,8 @@ import express, {
   type Request,
   type Response,
 } from "express";
-import { pool } from "./db";
 import { userRoute } from "./modules/user/user.route";
+import { profileRoute } from "./modules/profile/profile.route";
 
 const app: Application = express();
 
@@ -21,14 +21,6 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use("/api/users", userRoute);
-
-
-
-
-
-
-
-
-
+app.use("/api/profile", profileRoute);
 
 export default app;
